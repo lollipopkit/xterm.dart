f289cc3d3fd264598c0bbf413da08ba599c00ef9
lollipopkit <lollipopkit@qq.com>
2023-08-02 19:57:56 +0800

    fix: IME `enter` doesn't work

--- lib/src/terminal_view.dart
+++ lib/src/terminal_view.dart
@@ -269,7 +269,7 @@ class TerminalViewState extends State<TerminalView> {
         onComposing: _onComposing,
         onAction: (action) {
           _scrollToBottom();
-          if (action == TextInputAction.done) {
+          if (action == TextInputAction.done || action == TextInputAction.newline) {
             widget.terminal.keyInput(TerminalKey.enter);
           }
         },
